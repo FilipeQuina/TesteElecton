285a29cf866caf4dc4cdc21c79c6fe0453c9c052
FilipeQuina <filipe.pacheco00@gmail.com>
2018-09-16 18:46:27 -0300

    teste

---
+++ controller.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#btn_cancel').click(function () {
+        console.log("fechar")
+    });
+    $('#btn_plus').click(function () {
+        console.log("max")
+    });
+    $('#btn_minus').click(function () {
+        console.log("min")
+    });
+
+});
