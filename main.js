7574b9af71e714d4c090c239456a6f72a1dc5214
Filipe Pacheco <filipe.pacheco@smartnx.local>
2019-04-17 13:17:59 -0300

    Atualização

--- main.js
+++ main.js
@@ -7,13 +7,19 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1300, height: 700,frame:false})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-   mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -31,19 +37,15 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
+  // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
+  // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+  if (mainWindow === null) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
--- renderer.js
+++ renderer.js
@@ -1,3 +1,34 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var loki = require('lokijs');
+var db = new loki('db.json');
+var clientes = db.addCollection('clientes');
+//clientes.insert({
+//    nome:"filipe",
+//    email:"fili@gmail.com"
+//});
+//db.save()
+
+function ready(fn) {
+    if(document.readyState != 'loading'){
+        fn();
+    }
+    else{
+        document.addEventListener('DOMContentLoaded',fn);
+    }
+}
+ready(function(){
+    document.querySelector('#salvar').addEventListener('click',function(e){
+        e.preventDefault();
+        let data ={
+            nome:document.querySelector('#nome').value,
+            cpf:document.querySelector('#cpf').value,
+            telefone:document.querySelector('#telefone').value
+        };
+        clientes.insert(data);
+        db.save();
+        alert('salvo com sucesso');
+        document.querySelector('#cadastro-cliente').reset();
+    });
+});
